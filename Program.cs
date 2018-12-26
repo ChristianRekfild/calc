bb4c79e1b013f57165662abe18dbb4019d044d11
ChristianRekfild <ChrisRekfild@gmail.com>
2018-12-26 10:29:15 +0500

    StringCalc V1.1@

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+
+namespace String_Calc {
+    class Program {
+        static void Main(string[] args) {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Добро пожаловать в программу калькулятор v1.1");
+            Console.WriteLine("Введите выражение для вычисления");
+
+            string math = Console.ReadLine();
+
+
+            //пусть оно вычисляет само себя. Не зря же я несколько часов искал и курил информацию, как сделать это всё проще.
+            string value = new DataTable().Compute(math, null).ToString();
+
+
+
+            bool flag = true;
+            foreach (char c in value) {
+                if (!char.IsDigit(c) && c != '.') {
+                    flag = false;
+                }
+            }
+
+            if (flag) {
+                Console.WriteLine(value);
+            } else {
+                Console.WriteLine("Ошибка вычисления - введены некорректные данные или присутствует деление на ноль");
+            }
+
+
+        }
+    }
+}
